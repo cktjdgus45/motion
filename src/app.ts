a9889f798ca02d8e5dec9954a4f2677c4abf2db8
pooh <cktjdgus45@naver.com>
2021-03-02 10:39:02 +0900

    Add Empty modal Dialog that can be closed

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import { Component } from './components/component.js';
+import { InputDialog } from './components/dialog/dialog.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { Composable, PageComponent } from './components/page/page.js';
+import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
 
 class App {
     private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
-        this.page = new PageComponent();
+        this.page = new PageComponent(PageItemComponent);
         this.page.attachTo(appRoot);
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
@@ -22,6 +23,22 @@ class App {
 
         const video = new VideoComponent('Video Title', 'https://youtu.be/AjZt2vnsTpQ');
         this.page.addChild(video);
+
+        const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
+        imageBtn.addEventListener('click', () => {
+            const dialog = new InputDialog();
+
+            dialog.setOnCloseListener(() => {
+                dialog.removeFrom(document.body);
+            })
+
+            dialog.setOnSubmitListener(() => {
+                //섹션을 만들어서 페이지에 추가 해준다.
+                dialog.removeFrom(document.body);
+            })
+
+            dialog.attachTo(document.body);
+        })
     }
 }
 
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,9 +4,16 @@ export interface Composable {
     addChild(child: Component): void;
 }
 
+interface SectionContainer extends Component, Composable {
+    setOnCloseListener(listener: OnCloseListener): void;
+}
 type OnCloseListener = () => void;
 
-class PageItemComponent extends BaseComponent<HTMLElement>{
+type SectionContainerConstructor = {
+    new(): SectionContainer;
+}
+
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer {
     private closeListener?: OnCloseListener;
 
     constructor() {
@@ -32,12 +39,12 @@ class PageItemComponent extends BaseComponent<HTMLElement>{
 }
 
 export class PageComponent extends BaseComponent<HTMLUListElement>{
-    constructor() {
+    constructor(private pageItemConstructor: SectionContainerConstructor) {
         super('<ul class="page"></ul>');
     }
 
     addChild(section: Component) {
-        const item = new PageItemComponent();
+        const item = new this.pageItemConstructor();
         item.addChild(section);
         item.attachTo(this.element, 'beforeend');
         item.setOnCloseListener(() => {
